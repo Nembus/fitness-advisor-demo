79ce70a58c58c0f2f1de26166d48cb97cb935434
timk <timurlan77@gmail.com>
2024-06-11 12:29:29 -0400

    display selected solution once it's chosen for clear context.

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -4,11 +4,13 @@ export type RegimenItem = {
 }
 type Props = {
   regimen: RegimenItem[];
+  selectedSolution: string;
 }
 
-const Plan = ({regimen}: Props) => {
+const Plan = ({regimen, selectedSolution}: Props) => {
+  console.log({selectedSolution});
   return (<div className={'max-w-3xl'}>
-    <h1 className={'text-xl font-bold w-full text-center pb-4'}>My Regimen:</h1>
+    <h1 className={'text-xl font-bold w-full text-center pb-4'}>My Regimen for {selectedSolution}:</h1>
     <div>
       {(regimen ?? []).map(({day, instructions}: RegimenItem, idx: number) => (<div key={idx} className={'py-2 flex'}>
         <div className={'font-bold pr-2 w-fit flex-shrink-0'}>{day}:</div>
